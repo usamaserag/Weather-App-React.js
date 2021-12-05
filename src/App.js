0cc63144eec76e0625d0a58f4a4d7dd66d771f24
usamaserag <usamaserageldeen@gmail.com>
2021-12-05 02:28:47 +0200

    Created the start of app component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import ChooseCity from "./components/ChooseCity";
 import './App.css';
 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  box-shadow: 0 0 18px 0 rgba(0, 0, 0, 0.2);
+  width: 800px;
+  border-radius: 25px;
+  background-color: #1e232c;
+  padding: 10px 20px;
+  height: 500px;
+`;
+
 function App() {
+  const [city, setCity] = useState("")
   return (
-    <div className="App">
-      Weather app
-    </div>
+    <Container>
+      <ChooseCity city={city} setCity={setCity} />
+    </Container>
   );
 }
 
---
+++ src/components/ChooseCity/index.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+
+const SearchInputStyle = styled.input`
+  width: 100%;
+  outline: none;
+  border-radius: 25px;
+  border: none;
+  padding: 10px;
+`;
+
+const ButtonStyle = styled.input`
+  outline: none;
+  width: 100%;
+  border: none;
+  border-radius: 25px;
+  padding: 10px;
+  background-image: linear-gradient(135deg, #72edf2 10%, #5151e5 100%);
+  color: white;
+  font-weight: 700;
+  cursor: pointer;
+  transition: transform 200ms ease;
+  margin-top: 10px;
+  &:hover {
+    transform: scale(0.97);
+  }
+`;
+
+const ChooseCity = ({city, setCity}) => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setCity("")
+        console.log(city)
+    }
+
+  return (
+    <>
+      <form style={{margin: "auto"}} onSubmit={handleSubmit}>
+        <SearchInputStyle
+          type="search"
+          placeholder="Search for location..."
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+          required
+        />
+        <ButtonStyle type="submit" value="SEARCH" />
+      </form>
+    </>
+  );
+};
+
+export default ChooseCity;
